af3f1e9084f34d200151487b53b2adadd9172e2e
Ali <ga_hammas@esi.dz>
2020-03-31 18:20:52 +0100

    Getting Started

--- app/src/main/java/com/example/coronawatch/MainActivity.kt
+++ app/src/main/java/com/example/coronawatch/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.coronawatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startBtn = startButtonView
+
+
     }
 }
